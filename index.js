9ff8c6f115b1b597aa3124b8313bde50dedca96c
Steve Romm <smr310@gmail.com>
2018-02-18 14:44:12 -0500

    added youtube video

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var loadquote = function() {
     $.ajax({
-            type: "GET",
-            url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies&count=1",
-            data: {},
-            dataType: 'json',
-            success: function(data) {
+        type: "GET",
+        url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies&count=1",
+        data: {},
+        dataType: 'json',
+        success: function(data) {
             $("#quote-text").html(data.quote);
-            $("#quote-author").html(data.author);  
-            },
-            error: function(err) { alert("Internet Disconnected!"); },
-            beforeSend: function(xhr) {
-                xhr.setRequestHeader("X-Mashape-Authorization", "ryB0q5KlGGmshFp7gYxbKmNNlYwZp1VR487jsnByzMIGHEKhc3");
-            }
+            $("#quote-author").html(data.author);
+            youtubeCall(data.quote, data.author);
+        },
+        error: function(err) { alert("Internet Disconnected!"); },
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("X-Mashape-Authorization", "ryB0q5KlGGmshFp7gYxbKmNNlYwZp1VR487jsnByzMIGHEKhc3");
+        }
     });
 };
 
@@ -19,4 +20,30 @@ $("#get-quote-button").on("click", function() {
     loadquote();
 })
 
-//testing code 
+function youtubeCall(quote, movie) {
+    const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+    const query = {
+        q: quote,
+        per_page: 1,
+        part: 'snippet',
+        key: "AIzaSyDW01WDj_JY47WKZmAJ14fj7TXaiM-nOZM"
+
+    }
+    $.getJSON(YOUTUBE_SEARCH_URL, query, getJSONCB);
+}
+
+function getJSONCB(data) {
+    console.log(data);
+    $("#video").html(
+        `<iframe allowFullScreen="true" webkitallowfullscreen="true" width="420" height="315" src=https://www.youtube.com/embed/${data.items[0].id.videoId}?autoplay=1></iframe>`
+    );
+    console.log(data)
+}
+
+// {/* <iframe width="420" height="315"
+// src="https://www.youtube.com/embed/tgbNymZ7vqY">
+// </iframe> */}
+
+//this is the link: https://www.youtube.com/watch?v=
+// add id after = 
